37bea7d2a5c07c9867a36c81931054bf208d543c
MyeongJun Go <myeongjun.ko@gmail.com>
2020-02-04 23:35:22 +0900

    added user, post and comment data models

---
+++ djangogram/posts/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from djangogram.users import models as user_model
+
+# Create your models here.
+class TimeStamedModel(models.Model):
+    create_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class Post(TimeStamedModel):
+    author = models.ForeignKey(
+                user_model.User, 
+                null=True, 
+                on_delete=models.CASCADE, 
+                related_name= 'post_author'
+            )
+    image = models.ImageField(blank=True)
+    cpation = models.TextField(blank=True)
+    image_likes = models.ManyToManyField(user_model.User, related_name='post_image_likes')
+
+
+class Comment(TimeStamedModel):
+    author = models.ForeignKey(
+            user_model.User, 
+            null=True, 
+            on_delete=models.CASCADE, 
+            related_name= 'post_author'
+        )
+    posts = models.ForeignKey(
+            Post, 
+            null=True, 
+            on_delete=models.CASCADE, 
+            related_name= 'comment_post'
+        )
+    contents = models.TextField(blank=True)
--- djangogram/users/models.py
+++ djangogram/users/models.py
@@ -1,14 +1,28 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db import models
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
 
 class User(AbstractUser):
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('C', 'Custom'),
+    ]
 
     # First Name and Last Name do not cover name patterns
     # around the globe.
-    name = CharField(_("Name of User"), blank=True, max_length=255)
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
+    user_name = models.CharField(blank=True, max_length=255)
+    profile_photo = models.ImageField(blank=True)
+    website = models.URLField(blank=True)
+    bio = models.TextField(blank=True)
+    email = models.CharField(blank=True, max_length=255)
+    phone_number = models.CharField(blank=True, max_length=255)
+    gender = models.CharField(blank=True, choices=GENDER_CHOICES, max_length=255)
+    followers = models.ManyToManyField("self")
+    following = models.ManyToManyField("self")
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
