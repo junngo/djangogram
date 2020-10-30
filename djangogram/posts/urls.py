4689ed1678d628f1419acc2d0512948a704d2a4e
MyeongJun Go <myeongjun.ko@gmail.com>
2020-10-30 15:52:34 +0900

    creating post create api

--- djangogram/posts/urls.py
+++ djangogram/posts/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'posts'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create/', views.post_create, name='post_create'),
 ]
--- djangogram/posts/views.py
+++ djangogram/posts/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from djangogram.users.models import User as user_model
+
+from . import models
 
 # Create your views here.
 def index(request):
     return render(request, 'posts/base.html')
+
+def post_create(request):
+    if request.method == 'GET':
+        return render(request, 'posts/post_create.html')
+
+    elif request.method == 'POST':
+        if request.user.is_authenticated:
+            user = get_object_or_404(user_model, pk=request.user.id)
+            image = request.FILES['image']
+            caption = request.POST['caption']
+
+            new_post = models.Post.objects.create(
+                author = user,
+                image = image,
+                caption = caption
+            )
+            new_post.save()
+
+            return render(request, 'post/base.html')
+
+        else:
+            return render(request, 'users/main.html')
