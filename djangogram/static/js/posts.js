bc0663f7691d10a4e25c138e5da9f063559a0f22
MyeongJun Go <myeongjun.ko@gmail.com>
2022-03-05 19:20:39 +0900

    control html by js

--- djangogram/static/js/posts.js
+++ djangogram/static/js/posts.js
@@ -1,3 +1,16 @@
 const handleLikeClick = (buttonId) => {
     console.log(buttonId);
+
+    const likeButton = document.getElementById(buttonId);
+    console.log(likeButton);
+    const likeIcon = likeButton.querySelector("i");
+    console.log(likeIcon);
+    likeIcon.classList.replace("fa-heart-o", "fa-heart")
+    // likeIcon.innerText = "good";
+
+
+
+    // 서버로 좋아요 api를 호출
+
+    // 결과를 받고 html(좋아요 하트) 모습을 변경
 }
